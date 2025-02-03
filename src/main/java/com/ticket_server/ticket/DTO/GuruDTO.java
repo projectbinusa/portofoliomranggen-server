cc35a2a60151b3d197559c395608ee25247f5058
unknown <SuryaTanjungcool>
2025-02-03 12:53:41 +0700

    Memperbaiki api guru

--- src/main/java/com/ticket_server/ticket/DTO/GuruDTO.java
+++ src/main/java/com/ticket_server/ticket/DTO/GuruDTO.java
@@ -1,5 +1,13 @@
 package com.ticket_server.ticket.DTO;
 
+import com.ticket_server.ticket.model.Guru;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GuruDTO {
     private Long id;
     private Long idAdmin;
@@ -10,68 +18,15 @@ public class GuruDTO {
     private Integer tahunDiterima;
     private Integer lamaKerja;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getIdAdmin() {
-        return idAdmin;
-    }
-
-    public void setIdAdmin(Long idAdmin) {
-        this.idAdmin = idAdmin;
-    }
-
-    public String getNamaGuru() {
-        return namaGuru;
-    }
-
-    public void setNamaGuru(String namaGuru) {
-        this.namaGuru = namaGuru;
-    }
-
-    public String getNip() {
-        return nip;
-    }
-
-    public void setNip(String nip) {
-        this.nip = nip;
-    }
-
-    public String getAlamat() {
-        return alamat;
-    }
-
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-
-    public String getNomerHp() {
-        return nomerHp;
-    }
-
-    public void setNomerHp(String nomerHp) {
-        this.nomerHp = nomerHp;
-    }
-
-    public Integer getTahunDiterima() {
-        return tahunDiterima;
-    }
-
-    public void setTahunDiterima(Integer tahunDiterima) {
-        this.tahunDiterima = tahunDiterima;
-    }
-
-    public Integer getLamaKerja() {
-        return lamaKerja;
-    }
-
-    public void setLamaKerja(Integer lamaKerja) {
-        this.lamaKerja = lamaKerja;
+    // Constructor untuk konversi dari entity Guru ke DTO
+    public GuruDTO(Guru guru) {
+        this.id = guru.getId();
+        this.idAdmin = guru.getAdmin().getId();
+        this.namaGuru = guru.getNamaGuru();
+        this.nip = guru.getNip();
+        this.alamat = guru.getAlamat();
+        this.nomerHp = guru.getNomerHp();
+        this.tahunDiterima = guru.getTahunDiterima();
+        this.lamaKerja = guru.getLamaKerja();
     }
 }
