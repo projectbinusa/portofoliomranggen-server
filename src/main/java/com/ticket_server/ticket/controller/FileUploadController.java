d193fd6db77f2b1e48f4eda55446889f257dde67
IzzulFahmiHaq <izzulfahmi398@gmail.com>
2025-02-19 12:21:59 +0700

    up

--- src/main/java/com/ticket_server/ticket/controller/FileUploadController.java
+++ src/main/java/com/ticket_server/ticket/controller/FileUploadController.java
@@ -19,28 +19,22 @@ public class FileUploadController {
     @PostMapping("/image")
     public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file) {
         try {
-            // Cek apakah file kosong
+
             if (file.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("File tidak boleh kosong");
             }
 
-            // Membuat nama file unik
             String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
-            // Menentukan path penyimpanan
             Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
             Path filePath = uploadPath.resolve(fileName);
 
-            // Buat folder jika belum ada
             Files.createDirectories(uploadPath);
 
-            // Simpan file ke sistem
             Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
 
-            // URL akses gambar
             String imageUrl = "http://localhost:4321/uploads/" + fileName;
 
-            // Kembalikan URL gambar yang dapat diakses
             return ResponseEntity.ok(Map.of("imageUrl", imageUrl));
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Gagal mengunggah gambar: " + e.getMessage());
